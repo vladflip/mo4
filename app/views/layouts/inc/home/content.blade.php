4e6e5e5b55cd79ba3a73bd1da2a7f64b75ffc10b
Casper <caspernaruto@qip.ru>
2015-04-01 02:10:18 +0300

    test

---
+++ app/views/layouts/inc/home/content.blade.php
@@ -0,0 +1,20 @@
+<div class="content">
+	<div class="left">
+		<div class="news"></div>
+		<div class="news"></div>
+		<div class="news"></div>
+		<div class="news"></div>
+
+		<div class="pagination"></div>
+	</div>
+	<div class="right">
+		<div class="search"></div>
+		<div class="about"></div>
+		<div class="follow"></div>
+		<div class="popular"></div>
+		<div class="latest"></div>
+		<div class="topics"></div>
+		<div class="authors"></div>
+		<div class="comments"></div>
+	</div>
+</div>
